86f2c9f99815dbd41306280410f65436b72feeae
12MaXX43 <newm33825@gmail.com>
2026-01-27 17:03:53 +0100

    fifth commit

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>шарлатан2</h1>
     <form action="index2.php" method="post">
         <select name="number">
             <?php
-            for ($a = 1,$b = 10; $a <= 100; $a+=10, $b+=10) {
+            for ($a = 1, $b = 10; $a <= 100; $a += 10, $b += 10) {
                 echo "<option value=" . $a . -$b . ">" . $a . -$b . "</option>";
             }
             ?>
@@ -18,4 +20,5 @@
         <button type="submit">qwerty</button>
     </form>
 </body>
+
 </html>
--- index2.php
+++ index2.php
@@ -1,29 +1,31 @@
 <?php
-    $randNumber = rand(1, 100);
-    $range = explode( '-', $_POST['number']);
-    var_dump($randNumber, $range[1] );
-    if($range[0] <= $randNumber && $range[1] >= $randNumber){
-        echo 'jh';
-    }else{
-        echo 'loh';
-    }
-    // $start
-    if(intval($randNumber / 10) * 10 == $randNumber){
-        $finish = intval($randNumber / 10) * 10;
-    }else{
-        $finish = (intval($randNumber / 10) + 1) * 10;
-    }
+$randNumber = rand(1, 100);
+$range = explode('-', $_POST['number']);
+if ($range[0] <= $randNumber && $range[1] >= $randNumber) {
+    echo 'jh';
+} else {
+    echo 'loh';
+}
+if (intval($randNumber / 10) * 10 == $randNumber) { //провіряє нолік в кінці
+    $start = intval($randNumber / 10) * 10 - 9;
+    $finish = intval($randNumber / 10) * 10;
+} else { //нема ноліка в кінці
+    $start = intval($randNumber / 10) * 10 + 1;
+    $finish = (intval($randNumber / 10) + 1) * 10;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-     <form action="" method="post" id="numb">
+    <form action="" method="post" id="numb">
         <select name="number">
             <?php
             for ($a = 1; $a <= 10; $a++) {
@@ -33,4 +35,5 @@
         </select>
     </form>
 </body>
+
 </html>
